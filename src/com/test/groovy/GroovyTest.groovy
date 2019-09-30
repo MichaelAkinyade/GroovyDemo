ce8d0dbddc93188971e4d45583aa28aa82d3bd91
MichaelAkinyade <46787117+MichaelAkinyade@users.noreply.github.com>
2019-09-30 13:43:35 +0200

    changes

--- src/com/test/groovy/GroovyTest.groovy
+++ src/com/test/groovy/GroovyTest.groovy
@@ -8,7 +8,7 @@ class GroovyTest {
 	static void main(args) {
 
 		
-		def thirdP ="C:\\Users\\michael.akinyade\\OneDrive - GlobalCapital Malta\\michael.akinyade\\Desktop\\THIRD_PARTIES_ACTIVE_ADDRESS-24302-2019-03-17.xml"
+		def thirdP ="C:\\Users\\m\\OneDrive - GlobalCapital Malta\\m\\Desktop\\THIRD_PARTIES_ACTIVE_ADDRESS-24302-2019-03-17.xml"
 		
 		def entries = new XmlParser().parse(thirdP)
 		
